8ff48999d1bdf16d5a9997d3222044577cb67c88
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-02-05 17:38:20 +0400

    add the main page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { MainComponent } from './main.component';
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    SharedModule
   ]
 })
 export class MainModule { }
